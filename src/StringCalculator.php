b709a5c3bbb817c502cdfd40cec99a08bea69310
EthanTurrillas <ethanturrillas8@gmail.com>
2025-03-21 13:52:34 +0100

    Kata Calculator #1

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,58 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+
+    private function getDelimitador(&$cadena) {
+        if (strpos($cadena, "//") === 0) {
+            list($delimitador, $cadena) = explode("\n", substr($cadena, 2), 2);
+            return preg_quote($delimitador, '/');
+        }
+        return ",|\n";
+    }
+
+    private function obtenerNumeros($cadena, $delimitador) {
+        return preg_split("/" . $delimitador . "/", $cadena);
+    }
+
+    private function convertirYSumar($numeros) {
+        return array_sum(array_map('intval', $numeros));
+    }
+
+    private function add($cadena) {
+        $delimitador = $this->getDelimitador($cadena);
+        $numeros = $this->obtenerNumeros($cadena, $delimitador);
+        return $this->convertirYSumar($numeros);
+    }
+
+    private function negative($numbers): bool{
+        $delimitador = $this->getDelimitador($numbers);
+        $numeros = $this->obtenerNumeros($numbers, $delimitador);
+        foreach ($numeros as $numero) {
+            if ($numero < 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function negativeNumbers($numbers): string
+    {
+        $delimitador = $this->getDelimitador($numbers);
+        $numeros = $this->obtenerNumeros($numbers, $delimitador);
+        $error = "Negativos no soportados: ";
+        foreach ($numeros as $numero) {
+            if ($numero < 0) {
+                $error .= $numero;
+            }
+        }
+        return $error;
+    }
+
+
+    public function intAdd(string $numbers): int|string{
+        if (empty($numbers)) return 0;
+        elseif ($this->negative($numbers)) return $this->negativeNumbers($numbers);
+        elseif (strlen($numbers) < 2 ) return $numbers;
+        else return $this->add($numbers);
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,68 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    /**
+     * @test
+     */
+    public function noNumberGivenReturn0(){
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("");
+        $this->assertEquals(0, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenSingleNumberReturnSameNumber(){
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("1");
+        $this->assertEquals(1, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function given2NumbersReturnAdd(){
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("1,2");
+        $this->assertEquals(3, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function given135Return9(){
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("1,3,5");
+        $this->assertEquals(9, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function given123WithBrakeLineReturn6(){
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("1\n2,3");
+        $this->assertEquals(6, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenNewDelimiterReturnAdd()
+    {
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("//;\n1;2");
+        $this->assertEquals(3, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenNegativeNumberReturnError()
+    {
+        $stringCalculator = new StringCalculator();
+        $result = $stringCalculator->intAdd("-1");
+        $this->assertEquals("Negativos no soportados: -1", $result);
+    }
 }
